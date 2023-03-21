5c79386f51e6c2a8448bf180f60d8e50b1ffe8a6
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2023-03-21 11:13:12 -0700

    updated

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
-/*
-   * main: prints _putchar
-   * return: always returns 0 (success)
+/**
+ * main - Print _putchar
+ * Return: Always 0
  */
-
 int main(void)
 {
 	_putchar('_');
