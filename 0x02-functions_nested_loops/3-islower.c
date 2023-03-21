88447313140fa66f13071ee7749ce94beadec6d7
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2023-03-21 12:16:03 -0700

    updated

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * int _islower - checks if a letter is lower
+ * return: returns 1 if the letter is lower and 0 if it is not
+ */
+int _islower(int c)
+{
+	if (islower(c)
+	{
+		return (1);
+	}
+
+	return (0);
+}
