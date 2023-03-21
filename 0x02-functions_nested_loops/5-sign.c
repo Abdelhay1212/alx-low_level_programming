1e83beb1d1e60968ccb6efe1ce96e1ec57eebbf4
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2023-03-21 13:27:41 -0700

    updated

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,17 +10,17 @@
 */
 int print_sign(int n)
 {
-	if (n > 0 )
+	if (n > 0)
 	{
 		printf("+");
 		return (1);
 	}
-	if (n < 0 )
+	else if (n < 0)
 	{
 		printf("-");
 		return (-1);
 	}
-	if (n == 0)
+	else
 	{
 		printf("0");
 		return (0);
