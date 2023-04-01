cae8c2b45d74ccc20480e41eec419f8bcc3262c3
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2023-03-31 17:18:40 -0700

    updated

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,20 +7,25 @@
  */
 char *cap_string(char *s)
 {
-	int i, j;
-	char a[] = " ,;.!?\"(){}";
+	int i = 0, j;
+	char a[] = " \t\n,;.!?\"(){}";
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (*(s + i))
 	{
-		for (j = 0; a[j] != '\0'; j++)
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
 		{
-			if (s[i] == a[j])
+			if (i == 0)
+				*(s + i) -= 'a' - 'A';
+			else
 			{
-				if (s[i] >= 'a' && s[i] <= 'z')
-					s[i] = s[i] - 32;
+				for (j = 0; j <= 12; j++)
+				{
+					if (a[j] == *(s + i - 1))
+						*(s + i) -= 'a' - 'A';
+				}
 			}
 		}
+		i++;
 	}
-
 	return (s);
 }
