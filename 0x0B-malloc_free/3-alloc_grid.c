9f84d25c9542da00d62fe8dc82a38a906cdf5734
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2023-04-14 23:47:10 -0700

    updated

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,7 +13,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	*array = (int *) malloc(sizeof(int *) * width);
+	array = (int *) malloc(sizeof(int *) * width);
 	if (*array == NULL)
 		return (NULL);
 
