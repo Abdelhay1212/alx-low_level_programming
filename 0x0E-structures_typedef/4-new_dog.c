93c1c5d5f5ddf4aa4dac457005aa11bc12442995
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2023-04-17 15:53:56 -0700

    updated

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "dog.h"
+
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t dog;
+
+	dog->name = 
+}
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+#ifndef DOG
+#define DOG
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+typedef struct dog dog_t;
+
+#endif
