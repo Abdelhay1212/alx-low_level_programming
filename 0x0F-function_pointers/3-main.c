4ddaad9ec7be965333a68d103630a0830ab8afb0
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2023-04-19 21:47:38 -0700

    updated

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "3-calc.h"
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[])
+{
+	int (*p)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	p = get_op_func(argv[2]);
+
+	if (!p)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	printf("%d\n", p(atoi(argv[1]), atoi(argv[3])));
+	return (0);
+}
