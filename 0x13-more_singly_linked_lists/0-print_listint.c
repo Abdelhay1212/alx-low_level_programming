016b9fc7e3bff02807d0342d4038f12a33b7cbc2
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2023-04-30 09:34:37 -0700

    updated

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_listint - prints elements of linked list
+ * @h: the head of the linked list
+ * Return: integer
+ **/
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
