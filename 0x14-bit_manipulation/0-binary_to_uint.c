b1a176975e30ebca7496a87b5c493319015f3177
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2023-05-06 06:04:21 -0700

    updated

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -19,7 +19,7 @@ unsigned int binary_to_uint(const char *b)
 	len--;
 	while (b[i] != '\0')
 	{
-		if (isalpha(b[i]))
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
 		if (b[i] == '1')
 			num += pow(2, len);
