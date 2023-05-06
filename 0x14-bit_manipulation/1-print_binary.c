1c21febd66f85904f60acbe45bf60683f643f3b0
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2023-05-06 09:39:46 -0700

    updated

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * print_binary - prints the binary representation of a number.
+ * @n: number to convert
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	int bite, binary = 0, i = 1;
+
+	while (n)
+	{
+		bite = n & 1;
+		n >>= 1;
+
+		binary += bite * i;
+		i *= 10;
+	}
+
+	printf("%d\n", binary);
+}
