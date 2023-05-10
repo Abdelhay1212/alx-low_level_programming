2f24acf27c386c718ce99cc3ec1bb33a28567d5a
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2023-05-10 07:55:20 -0700

    updated

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,8 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	wr = write(op, text_content, i);
+	if (text_content != NULL)
+		wr = write(op, text_content, i);
 
 	if (op == -1 || wr == -1)
 		return (-1);
