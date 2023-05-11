dc6af2c2fcf1a0e44a0c3ce64a707b3d2bf54d04
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2023-05-11 10:40:51 -0700

    updatedvi

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,10 +1,15 @@
 #include "main.h"
 
-
-int main(int argc, char *argv[])                                                
- {                                                                               
-         int src_file, dest_file, re;                                            
-         char buffer[1024];     
+/**
+ * main - program that copies the content of a file to another file.
+ * @argc: number of arguments passed to the function main
+ * @argv: stores the arguments passed to the function
+ * Return: 0
+ **/
+int main(int argc, char *argv[])
+{
+ 	int src_file, dest_file, re;
+ 	char buffer[1024];
 
 	if (argc != 3)
 	{
@@ -20,7 +25,7 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	dest_file = open(arv[2], O_CREAT | O_WRONLY | O_TRUNC);
+	dest_file = open(arv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (dest_file == -1)
 	{
 		dprintf(STDOUT_FILENO, "Error: Can't write to %s\n", argv[2]);
@@ -44,5 +49,23 @@ int main(int argc, char *argv[])
 					"Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
-	}while (re != -1)                                                
+
+	} while (re != -1)
+
+	c_src = close(src_file);
+	c_dest = close(dest_file);
+
+	if (c_src == -1)
+	{
+		dprintf(STDOUT_FILENO, "Error: Can't close fd %d\n", src_file);
+		exit(100);
+	}
+
+	if (c_dest == -1)
+	{
+		dprintf(STDOUT_FILENO, "Error: Can't close fd %d\n",dest_file);
+		exit(100);
+	}
+
+	return (0);
 }       
