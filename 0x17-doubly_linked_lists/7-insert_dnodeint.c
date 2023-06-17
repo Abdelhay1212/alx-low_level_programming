a559c60a4667bf2a651cb72225498758854d48f0
Abdelhay1212 <110763003+Abdelhay1212@users.noreply.github.com>
2023-06-17 19:15:06 +0200

    Update 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,78 +1,58 @@
 #include "lists.h"
 
-dlistint_t *create_node(int n)
-{
-	dlistint_t *node = malloc(sizeof(dlistint_t));
-
-	if (node == NULL)
-		return NULL;
-	node->n = n;
-	node->prev = NULL;
-	node->next = NULL;
-	return node;
-}
-
-void insert_after_node(dlistint_t *node, dlistint_t *new_node)
-{
-	new_node->next = node->next;
-	new_node->prev = node;
-	if (node->next)
-		node->next->prev = new_node;
-	node->next = new_node;
-}
-
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int len = 0;
-	dlistint_t *current = *h;
+	unsigned int len = 0, index = 0;
+	dlistint_t *current = NULL;
+	dlistint_t *temp = malloc(sizeof(dlistint_t));
 
-	if (current == NULL && idx > 0)
-		return NULL;
-
+	if (temp == NULL)
+		return (NULL);
+	temp->n = n;
+	temp->prev = NULL;
+	temp->next = NULL;
+	current = *h;
+	if (current == NULL)
+	{
+		*h = temp;
+		return (*h);
+	}
 	if (idx == 0)
 	{
-		dlistint_t *new_node = NULL;
-
-		*new_node = create_node(n)
-		if (new_node == NULL)
-			return NULL;
-		new_node->next = current;
-		if (current)
-			current->prev = new_node;
-		*h = new_node;
-		return new_node;
+		temp->next = current;
+		current->prev = temp;
+		*h = temp;
+		return (temp);
 	}
-
 	while (current)
 	{
-		if (len == idx - 1)
-		{
-
-		dlistint_t *new_node = NULL;
-
-		*new_node = create_node(n);
-		if (new_node == NULL)
-			return NULL;
-		insert_after_node(current, new_node);
-		return new_node;
-		}
 		len++;
 		current = current->next;
 	}
-
-	if (len == idx)
+	current = *h;
+	if (idx == len)
 	{
-		dlistint_t *new_node = NULL;
-
-		*new_node = create_node(n)
-		if (new_node == NULL)
-			return NULL;
-		if (current)
-			insert_after_node(current, new_node);
-		else
-			*h = new_node;
-		return new_node;
+		while (current->next)
+		{
+			current = current->next;
+		}
+		current->next = temp;
+		temp->prev = current;
+		return (temp);
 	}
-
-	return NULL;
+	while (current)
+	{
+		if (index == idx)
+		{
+			temp->next = current;
+			temp->prev = current->prev;
+			current->prev->next = temp;
+			current->prev = temp;
+			return (temp);
+		}
+		index++;
+		current = current->next;
+	}
+	free(temp);
+	return (NULL);
 }
