1a5b7af41af589e48fafb9b20a91a39258ab519b
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2023-06-17 11:00:31 -0700

    updated

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -11,6 +11,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	unsigned int idx = 0;
 	dlistint_t current = NULL;
 
+	if (*head == NULL)
+		return (NULL);
 	current = *head;
 	while (current)
 	{
