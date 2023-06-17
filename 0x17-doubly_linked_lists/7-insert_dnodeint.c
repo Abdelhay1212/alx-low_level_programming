b2d62f80919275b821b7dbebca2d0d2de7541a67
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2023-06-16 18:14:14 -0700

    updated

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -50,7 +50,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	index = 0;
 	while (current)
 	{
-		if (index = idx)
+		if (index == idx)
 		{
 			temp->next = current;
 			temp->prev = current->prev;
