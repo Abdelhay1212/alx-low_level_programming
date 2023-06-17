ecb130c3d1313d1d8116694b2cc889e2e9c28b93
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2023-06-17 07:15:46 -0700

    updated

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,7 +14,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *temp = malloc(sizeof(dlistint_t));
 
 	if (temp == NULL)
-		return (NULL);
+		return (NULL);  
 	temp->n = n;
 	temp->prev = NULL;
 	temp->next = NULL;
@@ -23,7 +23,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (current == NULL)
 	{
 		*h = temp;
-		return (*h);
+       		return (*h);
 	}
 
 	if (idx == 0)
@@ -43,7 +43,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	current = *h;
 	if (idx == len)
 	{
-		while (current)
+		while (current->next)
 		{
 			current = current->next;
 		}
@@ -59,8 +59,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		{
 			temp->next = current;
 			temp->prev = current->prev;
-			current->prev = temp;
 			current->prev->next = temp;
+			current->prev = temp;
 			return (temp);
 		}
 
