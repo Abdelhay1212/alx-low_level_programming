4c668134ddd05741c8100c9633c8ed2d07c1494e
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2023-07-09 09:45:57 -0700

    updated

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,25 +7,20 @@
 */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ht;
-	unsigned int i;
+	hash_table_t *hash_table;
+	unsigned long int i;
 
-	ht = malloc(sizeof(hash_table_t));
-	if (ht == NULL)
-		/* handle memory allocation error */
+	hash_table = malloc(sizeof(hash_table_t));
+	if (hash_table == NULL)
 		return (NULL);
-
-	ht->size = size;
-	ht->array = malloc(sizeof(hash_node_t) * size);
-	if (ht->array == NULL)
+	hash_table->size = size;
+	hash_table->array = malloc(size * sizeof(hash_node_t *));
+	if (hash_table->array == NULL)
 	{
-		/* handle memory allocation error */
-		free(ht);
+		free(hash_table);
 		return (NULL);
 	}
-
 	for (i = 0; i < size; i++)
-		ht->array[i] = NULL;
-
-	return (ht);
+		hash_table->array[i] = NULL;
+	return (hash_table);
 }
