62b7285b942040314129e7821dbadeaa1b71ed97
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2023-07-10 10:18:09 -0700

    updated

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,20 +1,13 @@
 #include "hash_tables.h"
 
 /**
- * key_index - gives you the index of a key.
- * @key: the key to generate its index
- * @size: the size of hash table
- * Return: index
+ * key_index - gives the index of a key
+ * @key: key to get index for
+ * @size: size of the hash table
+ *
+ * Return: index for the key
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int hash;
-	int c;
-
-	hash = 5381;
-	while ((c = *key++))
-	{
-		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-	}
-	return (hash % size);
+	return (hash_djb2(key) % size);
 }
---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,44 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds an element to the hash table.
+ * @ht: the hash table to insert element in
+ * @key: the key of the element
+ * @value: value
+ * Return: integer
+*/
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+    hash_node_t *temp = NULL, *current = NULL;
+    unsigned long int index;
+
+    if (ht == NULL || key == NULL || strlen(key) == 0)
+    {
+        return (0);
+    }
+
+    temp = malloc(sizeof(hash_node_t));
+    if (temp == NULL)
+    {
+        /*handle memory allocation failure*/
+        return (0);
+    }
+    temp->key = strdup(key);
+    temp->value = strdup(value);
+    temp->next = NULL;
+
+    index = key_index((const unsigned char *)key, ht->size);
+    if (ht->array[index] == NULL)
+    {
+        ht->array[index] = temp;
+        return (1);
+    }
+
+    current = ht->array[index];
+    while (current->next != NULL)
+    {
+        current = current->next;
+    }
+    current->next = temp;
+    return (1);
+}
