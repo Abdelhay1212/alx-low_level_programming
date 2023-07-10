e0df4ba93c41949d2e8c5025890539fb2f60deef
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2023-07-10 11:24:46 -0700

    updated

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,32 +9,27 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    hash_node_t *temp = NULL;
-    unsigned long int index;
+	hash_node_t *temp = NULL;
+	unsigned long int index;
 
-    if (ht == NULL || key == NULL || strlen(key) == 0)
-    {
-        return (0);
-    }
+	if (ht == NULL || key == NULL || strlen(key) == 0)
+	{
+		return (0);
+	}
 
-    temp = malloc(sizeof(hash_node_t));
-    if (temp == NULL)
-    {
-        /*handle memory allocation failure*/
-        return (0);
-    }
-    temp->key = strdup(key);
-    temp->value = strdup(value);
-    temp->next = NULL;
+	temp = malloc(sizeof(hash_node_t));
+	if (temp == NULL)
+	{
+		/*handle memory allocation failure*/
+		return (0);
+	}
 
-    index = key_index((const unsigned char *)key, ht->size);
-    if (ht->array[index] == NULL)
-    {
-        ht->array[index] = temp;
-        return (1);
-    }
+	temp->key = strdup(key);
+	temp->value = strdup(value);
+	temp->next = NULL;
 
-    temp->next = ht->array[index];
-    ht->array[index] = temp;
-    return (1);
+	index = key_index((const unsigned char *)key, ht->size);
+	temp->next = ht->array[index];
+	ht->array[index] = temp;
+	return (1);
 }
