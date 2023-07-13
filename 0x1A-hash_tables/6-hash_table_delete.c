ca8a4f06bbf7f1b23ae912864ac706fbb9af6950
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2023-07-12 19:50:11 -0700

    updated

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -7,21 +7,21 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-    hash_node_t *node, *tmp;
-    unsigned long int i;
+	hash_node_t *node, *tmp;
+	unsigned long int i;
 
-    for (i = 0; i < ht->size; i++)
-    {
-        node = ht->array[i];
-        while (node != NULL)
-        {
-            tmp = node;
-            node = node->next;
-            free(tmp->key);
-            free(tmp->value);
-            free(tmp);
-        }
-    }
-    free(ht->array);
-    free(ht);
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node != NULL)
+		{
+			tmp = node;
+			node = node->next;
+			free(tmp->key);
+			free(tmp->value);
+			free(tmp);
+		}
+	}
+	free(ht->array);
+	free(ht);
 }
