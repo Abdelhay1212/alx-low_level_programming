3891ecb0a62fc6b61993f3e5e7ce7377a7980bc6
Abdelhay1212 <110763003+Abdelhay1212@users.noreply.github.com>
2023-10-11 06:11:02 +0200

    Update 1-binary.c

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -9,40 +9,40 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-    size_t middle;
-    size_t left = 0;
-    size_t right = size - 1;
+	size_t middle;
+	size_t left = 0;
+	size_t right = size - 1;
 
-    if (array == NULL)
-        return (-1);
+	if (array == NULL)
+		return (-1);
 
-    while (left <= right)
-    {
-        size_t i;
-        middle = (left + right) / 2;
+	while (left <= right)
+	{
+		size_t i;
+		middle = (left + right) / 2;
 
-        printf("Searching in array: ");
-        for (i = left; i < right + 1; i++)
-        {
-            printf("%d", array[i]);
-            if (i != right)
-            {
-                printf(", ");
-            }
-        }
-        printf("\n");
+		printf("Searching in array: ");
+		for (i = left; i < right + 1; i++)
+		{
+			printf("%d", array[i]);
+			if (i != right)
+			{
+				printf(", ");
+			}
+		}
+		printf("\n");
 
-        if (array[middle] == value)
-            return (middle);
-        else if (array[middle] < value)
-        {
-            left = middle + 1;
-        }
-        else
-        {
-            right = middle - 1;
-        }
-    }
+		if (array[middle] == value)
+			return (middle);
+		else if (array[middle] < value)
+		{
+			left = middle + 1;
+		}
+		else
+		{
+			right = middle - 1;
+		}
+	}
 
-    return (-1);
+	return (-1);
 }
